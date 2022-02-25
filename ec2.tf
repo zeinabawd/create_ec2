2e4c7cef1a032c191a47384e437bec6ccf889a8a
zeinabawd <zeinabawd@gmail.com>
2022-02-25 14:30:15 +0200

    modify volume size

--- ec2.tf
+++ ec2.tf
@@ -4,6 +4,10 @@ resource "aws_instance" "ckad_try_ec2" {
   subnet_id              = aws_subnet.ckad_public_subnet1.id
   vpc_security_group_ids = [aws_security_group.ckad_sg.id]
   key_name               = var.key_name
+  root_block_device {
+    volume_size = 20 # in GB <<----- I increased this!
+    volume_type = "gp2"
+  }
   source_dest_check      = false
   monitoring             = true
   tags = {
